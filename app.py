d06d974632c5fbd3cbf37cd03f0b5f5c8c259ae4
Chukwuyem Obuseh <89673167+charleside2001@users.noreply.github.com>
2021-12-19 15:49:06 -0600

    commiting app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+from flask import Flask  # Import the Flask class
+app = Flask(__name__)    # Create an instance of the class for our use
+
+@app.route('/')
+def hello_world():
+    return 'Hello world'
